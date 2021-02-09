3fbd1f6b7c55d506d30a1084ac7dc9843ccbd251
BartekLD <73353322+BartekLD@users.noreply.github.com>
2021-02-09 02:12:26 +0100

    light

--- src/Exercises/PhongLight/quad_2.cpp
+++ src/Exercises/PhongLight/quad_2.cpp
@@ -10,10 +10,10 @@
 
 Quad_2::Quad_2() {
     std::vector<GLfloat> vertices = {
-            -1.0f,  -1.0f, 0.0f, 0.0f, 0.0f, 0.0f,0.0f,1.0f,
-            1.0f,   -1.0f, 0.0f, 1.0f, 0.0f, 0.0f,0.0f,1.0f,
-            1.0f,   1.0f,  0.0f, 1.0f, 1.0f, 0.0f,0.0f,1.0f,
-            -1.0f,  1.0f,  0.0f, 0.0f, 1.0f, 0.0f,0.0f,1.0f, //bottom
+            -1.0f,  -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,0.0f,1.0f,
+            1.0f,   -1.0f, 0.0f, 1.0f, 1.0f, 0.0f,0.0f,1.0f,
+            1.0f,   1.0f,  0.0f, 1.0f, 0.0f, 0.0f,0.0f,1.0f,
+            -1.0f,  1.0f,  0.0f, 0.0f, 0.0f, 0.0f,0.0f,1.0f, //bottom
            // 0.0f, 0.0f, 1.0f, 0.0, 1.0,
             /*-1.0f, -1.0f, 0.0, 0.5, 0.5, 1.0,
             1.0f, -1.0f, 0.0f, 0.5, 0.5, 1.0,*/ //first wall
@@ -28,9 +28,14 @@ Quad_2::Quad_2() {
              1.0f, -1.0f, 0.0, 0.81, 0.0, 0.5, *///fourth wall
     };
 
+    glGenBuffers(2, this->buffers_);
+    glBindBuffer(GL_ARRAY_BUFFER, this->buffers_[0]);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     std::vector<GLushort> indices = {
             0,1,2,
-            2,3,0, //bottom
+            0,2,3, //bottom
              };
 
     int width, height, n_channels;
@@ -50,11 +55,11 @@ Quad_2::Quad_2() {
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
     glBindTexture(GL_TEXTURE_2D,0);
 
-    GLuint v_buffer_handle;
-    glGenBuffers(1, &v_buffer_handle);
-    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->buffers_[1]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,indices.size() * sizeof(GLushort),indices.data(),
+                 GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
 
     /*std::vector<GLushort> indices = {
             0,2,1,3,1,2, //bottom
@@ -63,23 +68,17 @@ Quad_2::Quad_2() {
             10,11,12,
             13,15,14 };*/
 
-    GLuint idx_buffer_handle;
-    glGenBuffers(1,&idx_buffer_handle);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,idx_buffer_handle);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,indices.size() * sizeof(GLushort),indices.data(),
-                 GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
-    glGenVertexArrays(1, &vao_);
-    glBindVertexArray(vao_);
-    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+    glGenVertexArrays(1,&this->vao_);
+    glBindVertexArray(this->vao_);
+    glBindBuffer(GL_ARRAY_BUFFER, this->buffers_[0]);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE, 8* sizeof(GLfloat),reinterpret_cast<GLvoid *>(3*sizeof(GL_FLOAT)));
     glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2,3,GL_FLOAT,GL_FALSE, 8* sizeof(GLfloat),reinterpret_cast<GLvoid *>(3*sizeof(GL_FLOAT)));
+    glVertexAttribPointer(2,3,GL_FLOAT, GL_FALSE,8*sizeof(GL_FLOAT),reinterpret_cast<GLvoid *>(5*sizeof(GL_FLOAT)));
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,idx_buffer_handle);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,this->buffers_[1]);
     glBindVertexArray(0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
 }
